43716aab665c51bd4b569ca7bae8b3958aefdda4
Lokesh Medharimetla <73935267+lokesh-7977@users.noreply.github.com>
2024-10-20 00:57:39 +0530

    Update next.config.mjs

--- my-veda/next.config.mjs
+++ my-veda/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'], // Allows Next.js to optimize images hosted on Cloudinary
+  },
+};
 
 export default nextConfig;
